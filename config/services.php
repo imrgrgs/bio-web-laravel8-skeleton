a5e4fef7465acba8c19f044da5bc4177c23f6172
iomar <imrgrgs@hotmail.com>
2021-01-10 20:05:12 -0300

    services

--- config/services.php
+++ config/services.php
@@ -29,17 +29,17 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-//social midia login with laravel socialite
-'facebook' => [
-    'client_id' => env('FACEBOOK_CLIENT_ID'),
-    'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-    'redirect' => env('APP_URL').'/portal/sociallogin/facebook/callback',
-],
+    //social midia login with laravel socialite
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => env('APP_URL') . '/portal/sociallogin/facebook/callback',
+    ],
 
-'google' => [
-    'client_id' => env('GOOGLE_CLIENT_ID'),
-    'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-    'redirect' => env('APP_URL').'/portal/sociallogin/google/callback',
-],
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('APP_URL') . '/portal/sociallogin/google/callback',
+    ],
 
 ];
